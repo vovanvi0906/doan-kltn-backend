4cb3a739c4d903912ea1d7fe9666f0b878a6a917
vanvi77 <vovanvi0906@gmail.com>
2026-08-27 14:27:16 +0700

    chore(database): fix dotenv loading and integrate PrismaModule into AppModule

--- src/app.module.js
+++ src/app.module.js
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './health/health.module';
+import { PrismaModule } from './infrastructure/database/prisma.module';
 
 @Module({
-  imports: [HealthModule],
+  imports: [HealthModule, PrismaModule],
 })
-export class AppModule {}
+export class AppModule { }
